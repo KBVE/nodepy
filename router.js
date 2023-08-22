e40d8dcc8dda5795495b44cfe15ec6174e78bf1f
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-08-22 04:20:17 -0400

    Update router.js

--- router.js
+++ router.js
@@ -48,6 +48,11 @@ router.all("/app/api/:token/:file", async (ctx, next) => {
   }
 });
 
+router.get("/vardump", (ctx, next) => {
+  ctx.status = 418;
+  ctx.body = `Var Dump ${process.env.KBVE_API}`;
+  ctx.app.emit('error', err, ctx);
+});
 router.all("/app/api/blueprint/:token", async (ctx, next) => {
   try {
     _v(`{r} -> blueprint -> token ${ctx.params.token}`);
