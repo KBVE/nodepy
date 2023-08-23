e01df1f1071868c159f8290d77154107ad57a15b
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-08-23 01:21:10 -0400

    Update ai.py

--- scripts/ai.py
+++ scripts/ai.py
@@ -1,9 +1,9 @@
 import llama_index
-import langchain
-import chromadb
-import OpenAIPydantic
-import openai
-import anthropic
+#import langchain
+#import chromadb
+#import OpenAIPydantic
+#import openai
+#import anthropic
 import os
 import json
 import re
