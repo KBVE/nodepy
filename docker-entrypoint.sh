39b2c9cb5fb6fc30dc32a742d85a51df249980c5
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-08-23 07:08:23 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,11 +11,11 @@ set -e
 # SQLITE PATCH
 wget https://www.sqlite.org/2023/sqlite-autoconf-3420000.tar.gz
 tar xvfz sqlite-autoconf-3420000.tar.gz
-sudo mv sqlite-autoconf-3420000 /usr/local/share/sqlite3
+mv sqlite-autoconf-3420000 /usr/local/share/sqlite3
 cd /usr/local/share/sqlite3
 ./configure
 make -j 1
-sudo make install
+make install
 
 
 # Evaluating passed command:
